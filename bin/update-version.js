673ec366d3ce43c079c7408bd6626cad3d8ce5f9
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-02-04 17:58:51 -0600

    As NPM Package: Noop: Add Whitespace

--- bin/update-version.js
+++ bin/update-version.js
@@ -4,4 +4,4 @@ const { version } = require('../package.json');
 const root = __dirname;
 const data = fs.readFileSync(root + '/../source/_imports/version.css', 'utf8');
 const result = data.replace(/{{ver}}/g, version);
-fs.writeFileSync(root + '/../source/_version.css', result,'utf8');
+fs.writeFileSync(root + '/../source/_version.css', result, 'utf8');
