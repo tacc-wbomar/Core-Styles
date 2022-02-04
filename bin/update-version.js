304500a3ca347065db6ea97b4ce5496ba2a96a17
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-02-04 14:10:09 -0600

    Major: Add version & filename w/ loud comments
    - version is added automatically (no new dependencies)
    - filename is added manually (until I know how to do auto)

---
+++ bin/update-version.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const {version} = require('../package.json');
+
+const root = __dirname;
+const data = fs.readFileSync(root + '/../source/_imports/version.css', 'utf8');
+const result = data.replace(/{{ver}}/g, version);
+fs.writeFileSync(root + '/../source/_version.css', result,'utf8');
