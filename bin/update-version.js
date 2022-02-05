261e0fdaecf52e71551904ebc90329cbf8678efe
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-02-04 18:35:19 -0600

    As NPM Package: (Almost) Noop: Rename variable

--- bin/update-version.js
+++ bin/update-version.js
@@ -3,5 +3,5 @@ const { version } = require('../package.json');
 
 const root = __dirname;
 const data = fs.readFileSync(root + '/../source/_imports/version.css', 'utf8');
-const result = data.replace(/{{ver}}/g, version);
-fs.writeFileSync(root + '/../source/_version.css', result, 'utf8');
+const newData = data.replace(/{{ver}}/g, version);
+fs.writeFileSync(root + '/../source/_version.css', newData, 'utf8');
