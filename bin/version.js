58858f93c89da9f3385ae8b3cbe15c7902594f3d
Wesley Bomar <wbomar@tacc.utexas.edu>
2022-03-21 18:45:19 -0500

    feat(version): (wip) use postcss-banner plugin

--- bin/version.js
+++ bin/version.js
@@ -1,31 +1,21 @@
 #!/usr/bin/env node
 
-/** Create CSS file to import that prints project version */
-
-const fs = require('fs');
+/** Create CSS version based on lifecycle app data and given data */
 
 const package = require(process.env.npm_package_json || '../package.json');
 
 
-
 /**
- * Create version stylesheet at specificed path
- * @param {string} outputPath - Output version file at which path
+ * Create version from app data and given data
  * @param {string} [buildId] - Any value to identify the build
  */
-function create(outputPath, buildId) {
-    // Get data
+function create(buildId) {
     const appName = package.name;
     const appVersion = buildId || package.version + '+';
     const appLicense = package.license;
     const appWebsite = package.homepage.replace('https://', '');
-    const fileContent = `/*! ${appName} ${appVersion} | ${appLicense} | ${appWebsite} */` + "\n";
 
-    // Tell user
-    console.log(`Updating CSS version to ${appVersion}`);
-
-    // Write version
-    fs.writeFileSync( outputPath, fileContent );
+    return `${appName} ${appVersion} | ${appLicense} | ${appWebsite}`;
 }
 
 
