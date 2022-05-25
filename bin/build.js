0cc819a918e7c638079733e7d11ce6a08692a0ec
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2022-05-25 16:57:33 -0500

    FP-1648: Build Individual Stylesheets — ⚠️ API Change (#23)
    * feat(fp-1648): allow build with custom glob input
    
    * fix(fp-1648)!: use input/ouput not input/output-dir
    
    BREAKING CHANGE: --input-dir & --output-dir now --input & --output
    
    * feat(fp-1648): build all stylesheets and commit dist/
    
    * feat(fp-1648)!: let user match pattern, drop --file-ext
    
    BREAKING CHANGE: --file-ext (and -e) removed

--- bin/build.js
+++ bin/build.js
@@ -11,23 +11,24 @@ process.env.FORCE_COLOR = true
 
 /**
  * Build stylesheets from source CSS
- * @param {string} inputDir - Parse CSS files from which directory
- * @param {string} outputDir - Output CSS files to which directory
+ * @param {string} input - Parse CSS files from which path
+ * @param {string} output - Output CSS files to which path
  * @param {object} [opts={}] - Options
- * @param {object} [opts.fileExt='css'] - Extension of CSS files to parse
+ * @param {string} [opts.baseMirrorDir] - Do not add this path when mirroring
  * @param {string} [opts.configDir] - Custom config directory
  * @param {boolean} [opts.verbose=false] - To print more info from build log
+ * @param {boolean} [opts.verbose=false] - To print more info from build log
  */
-function build(inputDir, outputDir, opts = {}) {
+function build(input, output, opts = {}) {
   // Get data
-  const fileExt = opts.fileExt || 'css';
   const configDir = opts.configDir || `${__dirname}/../`;
   const verbose = (opts.verbose === true) ? '--verbose' : '';
+  const base = (opts.baseMirrorDir) ? `--base "${opts.baseMirrorDir}"` : '';
 
   // Build command
-  const command = `postcss "${inputDir}/*.${fileExt}" --dir "${outputDir}" ${verbose} --config "${configDir}"`;
+  const command = `postcss "${input}" --dir "${output}" ${verbose} --config "${configDir}" ${base}`;
 
-  console.log(`Building stylesheets to ${outputDir}`);
+  console.log(`Building stylesheet(s) to ${output}`);
 
   // Run command
   cmd.runSync(command);
