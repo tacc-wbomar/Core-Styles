837ab3f9117a0979c3fbfb3cc14e88a1c2e6f472
Wesley Bomar <wbomar@tacc.utexas.edu>
2023-01-19 20:46:03 -0600

    fix(bin): build-id preprended with comma

--- bin/build.js
+++ bin/build.js
@@ -6,7 +6,8 @@ const { buildStylesheets } = require('../src/main');
 const mininmist = require('minimist');
 
 const ARGS = mininmist( process.argv.slice( 2 ) );
-const BUILD_ID = ARGS['build-id'] || '';
+// HELP: `ARGS['build-id']` is array (dunno why) of [',' '… the build id …']
+const BUILD_ID = ARGS['build-id'][1] || '';
 
 buildStylesheets('src/lib/_imports/**/*!(fractal.server.refresh).css', './dist', {
   baseMirrorDir: 'src/lib/_imports',
