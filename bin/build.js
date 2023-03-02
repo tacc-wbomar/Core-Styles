d731f8394282dd1fcd7cfde7d20b230a1626d34e
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2023-03-01 21:52:07 -0600

    feat: build has-dark-logo–themed stylesheets (#134)

--- bin/build.js
+++ bin/build.js
@@ -8,7 +8,21 @@ const mininmist = require('minimist');
 const ARGS = mininmist( process.argv.slice( 2 ) );
 const BUILD_ID = ARGS['build-id'] || '';
 
+/* Theme: (default) */
 buildStylesheets('src/lib/_imports/**/*!(fractal.server.refresh).css', './dist', {
   baseMirrorDir: 'src/lib/_imports',
   buildId: BUILD_ID
 });
+
+/* Theme: has-dark-logo */
+const hasDarkLogoBuildOpts = {
+  baseMirrorDir: 'src/lib/_imports',
+  buildId: BUILD_ID,
+  fileExt: '.theme-has-dark-logo.css',
+  customConfigs: [
+    `${__dirname}/../src/.postcssrc.has-dark-logo.yml`
+  ]
+};
+buildStylesheets('src/lib/_imports/core-styles.header.css', './dist', hasDarkLogoBuildOpts );
+buildStylesheets('src/lib/_imports/trumps/s-header.css', './dist', hasDarkLogoBuildOpts );
+buildStylesheets('src/lib/_imports/trumps/tacc-search-bar.css', './dist', hasDarkLogoBuildOpts );
